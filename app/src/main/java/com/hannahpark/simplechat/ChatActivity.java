4ad85c581acf1151ed94fe53bd932e80b1a436f1
Hannah Park <hannahpark@fb.com>
2018-07-09 11:05:22 -0700

    updated ChatActivity login

--- app/src/main/java/com/hannahpark/simplechat/ChatActivity.java
+++ app/src/main/java/com/hannahpark/simplechat/ChatActivity.java
@@ -1,13 +1,45 @@
 package com.hannahpark.simplechat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class ChatActivity extends AppCompatActivity {
+    static final String TAG = ChatActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        //User login
+        if(ParseUser.getCurrentUser() != null) {    //start with existing user
+            startWithCurrentUser();
+        } else {    //if not logged in, login as new anonymous user
+            login();
+        }
+    }
+
+    //get userId from the cached currentUser object
+    void startWithCurrentUser() {
+        //TODO:
+    }
+
+    //Create an anonymous user using ParseAnonymousUtils and set UserId
+    void login() {
+        ParseAnonymousUtils.logIn(new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Anonymous login failed: ", e);
+                } else {
+                    startWithCurrentUser();
+                }
+            }
+        });
     }
 }
